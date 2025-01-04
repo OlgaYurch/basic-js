27200deec8b934b10566cd4c3778cd07641f3ff1
Volha Yurchanka <volha@yurchanka.ru>
2025-01-04 21:26:03 +0300

    feat: add solution to dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return '';
+  };
+
+  const teamName = [];
+
+  members.forEach((member) => {
+    if(typeof member === 'string') {
+      teamName.push(member.trim().charAt(0).toUpperCase());
+    }
+  });
+  return teamName.sort().join('');
 }
 
 module.exports = {
